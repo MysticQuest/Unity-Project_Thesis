a9c256c7607ade149933266ae7812d92dd20f1d8
MysticQuest <l1skou@gmail.com>
2016-08-11 07:00:16 +0300

    fail commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    Camera cam1;
+
+	// Use this for initialization
+	void Start () {
+
+        cam1 = GetComponent<Camera>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        cam1.orthographicSize = (Screen.height / 100f) / 1.8f; 
+
+        if (target)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, 0.5f) + new Vector3(0, 0, -10);
+        }
+	
+	}
+}
---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttack : MonoBehaviour {
+
+    Rigidbody2D body1;
+    Animator anim;
+
+    void Start () {
+
+        body1 = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
+	
+	void Update () {
+        if (Input.GetMouseButton(0))
+        
+           anim.SetBool("IsAttacking",true);
+        
+        else 
+           anim.SetBool("IsAttacking",false);
+        
+	}
+}
