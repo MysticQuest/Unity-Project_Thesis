99ce8bfa5fb6b3a44137b3cd9060136f3258a27d
BuildTools <l1skou@gmail.com>
2017-02-08 21:22:47 +0200

    Main menu, Scene manager

---
+++ Assets/Scripts/Menu/UIFunctions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class UIFunctions : MonoBehaviour {
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("scene1");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    public void Help()
+    {
+
+    }
+}
